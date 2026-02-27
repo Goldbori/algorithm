5308184df7f06da316ac4271d936ab87b0cdf9c9
Goldbori <66511750+Goldbori@users.noreply.github.com>
2026-02-27 22:05:33 +0900

    [level 3] Title: 네트워크, Time: 0.05 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.cpp
+++ 프로그래머스/3/43162. 네트워크/네트워크.cpp
@@ -4,10 +4,35 @@
 
 using namespace std;
 
-int solution(int n, vector<vector<int>> computers) {
-    vector<int> visited(0, n);
+vector<int> visited;
+int nn;
+
+void dfs(int now, vector<vector<int>>& computers) {
     
-    for (int i = 0 ; i < visited.size() ; i++) {
-        cout << visited[i] << endl;
+    //방문한 곳? 생략
+    if (visited[now] == 1) return;
+    
+    //방문체크
+    visited[now] = 1;
+    
+    for (int i = 0 ; i < nn ; i++){
+        if (computers[now][i] == 1 && i != now) {
+            dfs(i, computers);
+        }
     }
+    
+    
+}
+int solution(int n, vector<vector<int>> computers) {
+    visited.assign(n, 0);
+    int cnt = 0;
+    nn = n;
+    for (int i = 0 ; i < n ; i++){
+        if (visited[i] == 0) {
+            dfs(i, computers);
+            cnt++;
+        }
+    }
+    
+    return cnt;
 }
