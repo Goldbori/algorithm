5aa97bd76453912dcdbaf329a07c4e58ea127b3f
SUN HYUN SEUNG <66511750+Goldbori@users.noreply.github.com>
2026-09-22 18:30:57 +0900

    [Passed] 문자열 두번 출력하기 (133ms, 10MB)

---
+++ trail1/문자열 두번 출력하기/print-string-twice.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException{
+        // Please write your code here.
+        // BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        // String tmp = br.readLine();
+        // bw.append(tmp);
+        // bw.append("\n");
+        // bw.append(tmp);
+        // bw.flush();
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        System.out.println(s);
+        System.out.println(s);
+        
+
+
+    }
+}
